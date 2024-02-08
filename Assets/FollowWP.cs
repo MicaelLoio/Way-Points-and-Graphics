1805669af6b958a6918ba02ff537fcdc72e1e787
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2024-02-08 15:49:03 +0000

    Traversing a Path
    there was a script error that i couldnt find out

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -6,7 +6,7 @@ public class FollowWP : MonoBehaviour
 {
     Transform goal;
         float speed = 5.0f;
-         float accuracy = 1.0f;
+         float accuracy = 5.0f;
         float rotSpeed = 2.0f;
 
     public GameObject wpManager;
@@ -22,6 +22,8 @@ public class FollowWP : MonoBehaviour
         wps = wpManager.GetComponent<WPManager>().waypoints;
         g = wpManager.GetComponent<WPManager>().graph;
         currentNode = wps[0];
+
+        Invoke("GoToRuin", 2);
     }
 
     public void GoToHeli()
@@ -41,8 +43,31 @@ public class FollowWP : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+            return;
+
+        if (Vector3.Distance(g.pathList[currentWP].getID().transform.position,
+                                this.transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getID();
+            currentWP++;
+        }
+
+        if (currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getID().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x,
+                                                this.transform.position.y,
+                                                goal.position.z);
+
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation,
+                                                 Quaternion.LookRotation(direction),
+                                                  Time.deltaTime * rotSpeed );
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
     }
 }
--- Assets/Graphs/Graph.cs
+++ Assets/Graphs/Graph.cs
@@ -7,7 +7,7 @@ public class Graph
     
     List<Edge> edges = new List<Edge>();
     List<Node> nodes = new List<Node>();
-    List<Node> pathList = new List<Node>();
+    public List<Node> pathList = new List<Node>();
 
     public Graph() { }
 
@@ -145,7 +145,7 @@ public class Graph
             if (l[i].f <= lowest)
             {
                  lowest = l[i].f;
-                iteratorCount = count
+                iteratorCount = count;
 
             }
 
