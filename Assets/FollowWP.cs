b26eddc22f411a2933263c40c8afaef6234f202f
MicaelLoio <131375089+MicaelLoio@users.noreply.github.com>
2024-02-08 01:50:12 +0000

    Slerping to the Direction of Travel
    Making the smoothness of the tank turn in the palmtree waypoint

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -11,6 +11,7 @@ public class FollowWP : MonoBehaviour
 
 
     public float speed = 10.0f;
+    public float rotSpeed = 10.0f;
 
 
 
@@ -29,7 +30,12 @@ public class FollowWP : MonoBehaviour
         if(currentWP >= waypoints.Length)
             currentWP = 0;
 
-        this.transform.LookAt(waypoints[currentWP].transform);
+       // this.transform.LookAt(waypoints[currentWP].transform);
+
+        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
+
         this.transform.Translate(0, 0, speed * Time.deltaTime );
      
     }
